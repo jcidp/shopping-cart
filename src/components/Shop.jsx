dedc568f6948b78a1a0a1b381a4a4f2f75dded51
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-11-24 21:03:21 -0600

    Create custom hook to fetch API data

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,17 +1,22 @@
+import useFetchAPI from "../hooks/useFetchAPI";
 import styles from "../styles/Shop.module.css";
 import ProductCard from "./ProductCard";
 
 const Shop = () => {
-    const products = [];
-    for (let i=0; i<6; i++) {
-        products.push(<ProductCard />);
-    }
+    const {data, error, isLoading} = useFetchAPI();
+    
+    if (error) return <><h2>Error loading data</h2></>;
+    if (isLoading) return <h2>Loading...</h2>
+    
+    const productIds = data.objectIDs.slice(0, 18);
     
     return (
         <section className="products">
             <h2>Products!</h2>
             <div className={styles.productGrid}>
-                {products}
+                {productIds.map(id => 
+                    <ProductCard key={id} id={id} />
+                )}
             </div>
         </section>
     );
---
+++ src/hooks/useFetchAPI.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const useFetchAPI = () => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchAPI() {
+            try {
+                const response = await fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11");
+                if(response.status >= 400) {
+                    throw new Error("server error");
+                }
+                const data = await response.json();
+                setData(data);
+            } catch(error) {
+                setError(error);
+                console.log(error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchAPI();
+    }, []);
+
+
+    return {data, error, isLoading};
+}
+
+export default useFetchAPI;
