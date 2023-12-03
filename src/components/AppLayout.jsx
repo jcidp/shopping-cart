9ff0137495c1095a2c4359376e3a3708021ac42c
Jonatan Cid <86532479+jcidp@users.noreply.github.com>
2023-12-02 21:11:42 -0600

    Fix ProductCard quantity reactivity bug & style it
    The sidebar cart item quantities weren't updating when clicking the
    "Add to Cart" button to add multiple quantities of the same item.
    
    Also added some basic styles to the cards.

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -22,7 +22,8 @@ function AppLayout() {
     }, [data])
 
     const handleAddToCart = (e, isEdit) => {
-        const id = +e.target.dataset.productId;
+        const btn = e.target.closest("button");
+        const id = +btn.dataset.productId;
         const quantity = +document.getElementById(`qty-${id}-${isEdit ? "cart" : ""}`).value;
         setProducts(products.map(product => {
             const newQuantity = isEdit ? quantity : product.cartQuantity + quantity;
@@ -32,7 +33,8 @@ function AppLayout() {
     };
 
     const handleRemoveFromCart = (e) => {
-        const id = +e.target.dataset.productId;
+        const btn = e.target.closest("button");
+        const id = +btn.dataset.productId;
         setProducts(products.map(product => {
             if (product.id !== id) return product;
             return {...product, cartQuantity: 0}
@@ -43,21 +45,23 @@ function AppLayout() {
 
     return (<>
         <header className={styles.header}>
-            <h1><Link to="/">MyShop</Link></h1>
-            <nav className={styles.nav}>
-                <ul className={styles.navMenu}>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li><Link to="/shop">Shop</Link></li>
-                    <li className={styles.centerContent}>
-                        <button className={styles.cart} onClick={toggleShowCart}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>cart-outline</title><path d="M17,18A2,2 0 0,1 19,20A2,2 0 0,1 17,22C15.89,22 15,21.1 15,20C15,18.89 15.89,18 17,18M1,2H4.27L5.21,4H20A1,1 0 0,1 21,5C21,5.17 20.95,5.34 20.88,5.5L17.3,11.97C16.96,12.58 16.3,13 15.55,13H8.1L7.2,14.63L7.17,14.75A0.25,0.25 0 0,0 7.42,15H19V17H7C5.89,17 5,16.1 5,15C5,14.65 5.09,14.32 5.24,14.04L6.6,11.59L3,4H1V2M7,18A2,2 0 0,1 9,20A2,2 0 0,1 7,22C5.89,22 5,21.1 5,20C5,18.89 5.89,18 7,18M16,11L18.78,6H6.14L8.5,11H16Z" /></svg>
-                            <span className={styles.cartItems}>{products.reduce((sum, product) => sum + product.cartQuantity, 0)}</span>
-                        </button>
-                    </li>
-                </ul>
-            </nav>
+            <div className={styles.container}>
+                <h1><Link to="/">MyShop</Link></h1>
+                <nav className={styles.nav}>
+                    <ul className={styles.navMenu}>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li><Link to="/shop">Shop</Link></li>
+                        <li className={styles.centerContent}>
+                            <button className={styles.cart} onClick={toggleShowCart}>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>cart-outline</title><path d="M17,18A2,2 0 0,1 19,20A2,2 0 0,1 17,22C15.89,22 15,21.1 15,20C15,18.89 15.89,18 17,18M1,2H4.27L5.21,4H20A1,1 0 0,1 21,5C21,5.17 20.95,5.34 20.88,5.5L17.3,11.97C16.96,12.58 16.3,13 15.55,13H8.1L7.2,14.63L7.17,14.75A0.25,0.25 0 0,0 7.42,15H19V17H7C5.89,17 5,16.1 5,15C5,14.65 5.09,14.32 5.24,14.04L6.6,11.59L3,4H1V2M7,18A2,2 0 0,1 9,20A2,2 0 0,1 7,22C5.89,22 5,21.1 5,20C5,18.89 5.89,18 7,18M16,11L18.78,6H6.14L8.5,11H16Z" /></svg>
+                                <span className={styles.cartItems}>{products.reduce((sum, product) => sum + product.cartQuantity, 0)}</span>
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
             {(location.pathname === "/" || location.pathname === "/shop") && products.length > 0 && showCart && (
                 <section className={styles.cartSideBar}>
                     <Cart products={products} error={error} isLoading={isLoading} handleAddToCart={handleAddToCart} handleRemoveFromCart={handleRemoveFromCart}/>
